49aa757c281db70f1df6e0dacb06ae559f80d9e2
damjanh <damjan.hudobreznik@gmail.com>
2020-02-14 23:14:09 +0100

    Implement logic to set header welcome text depending on time of day.

--- components/AppHeader.jsx
+++ components/AppHeader.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
 function AppHeader() {
+    const hours = new Date().getHours();
+    let timeOfDay;
+    if (hours < 12) {
+        timeOfDay = 'Morning';
+    } else if (hours >= 12 && hours < 17) {
+        timeOfDay = 'Afternoon';
+    } else {
+        timeOfDay = 'Night';
+    }
     return (
         <header className="header">
-            <h1>Hello</h1>
+            <h1>
+                {`Good ${timeOfDay}!`}
+            </h1>
         </header>
     );
 }
