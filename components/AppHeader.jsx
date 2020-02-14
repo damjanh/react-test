fc362b07062e26416999e334e7d24153e13bbb88
damjanh <damjan.hudobreznik@gmail.com>
2020-02-14 23:32:55 +0100

    Add dynamic header text style depending on the time of day.

--- components/AppHeader.jsx
+++ components/AppHeader.jsx
@@ -3,16 +3,20 @@ import React from 'react';
 function AppHeader() {
     const hours = new Date().getHours();
     let timeOfDay;
+    const welocomeTextStyle = {};
     if (hours < 12) {
         timeOfDay = 'Morning';
+        welocomeTextStyle.color = '#04756F';
     } else if (hours >= 12 && hours < 17) {
         timeOfDay = 'Afternoon';
+        welocomeTextStyle.color = '#2E0927';
     } else {
         timeOfDay = 'Night';
+        welocomeTextStyle.color = '#D90000';
     }
     return (
         <header className="header">
-            <h1>
+            <h1 style={welocomeTextStyle}>
                 {`Good ${timeOfDay}!`}
             </h1>
         </header>
