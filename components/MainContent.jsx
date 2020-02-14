885e0ed13b4f2c59e3b460746f3f59c4933b8436
damjanh <damjan.hudobreznik@gmail.com>
2020-02-14 23:54:06 +0100

    Implement todo list items and style.

--- components/MainContent.jsx
+++ components/MainContent.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 
 import AboutMe from './AboutMe';
 import CoderRequirements from './CoderRequirements';
+import TodoItem from './TodoItem';
 
 function MainContent() {
     return (
         <main>
             <AboutMe />
             <CoderRequirements />
+            <div className="todo-list">
+                <h4>TODO:</h4>
+                <TodoItem />
+                <TodoItem />
+                <TodoItem />
+            </div>
         </main>
     );
 }
---
+++ components/TodoItem.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const TodoItem = () => (
+    <div className="todo-item">
+        <input type="checkbox" />
+        <p>Placeholder text</p>
+    </div>
+);
+
+export default TodoItem;
