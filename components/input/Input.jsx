589ce2c4e6fec30be2e512ac49f8d57f990de550
damjanh <damjan.hudobreznik@gmail.com>
2020-02-20 20:51:49 +0100

    Implement multiple inputs and use single handleChange.

--- components/input/Input.jsx
+++ components/input/Input.jsx
@@ -6,19 +6,39 @@ class Input extends React.Component {
         this.state = {
             firstName: '',
             lastName: '',
+            isFriendly: false,
+            gender: '',
+            color: 'Red',
         };
         this.handleChange = this.handleChange.bind(this);
     }
 
     handleChange(event) {
-        const { name, value } = event.target;
-        this.setState({
-            [name]: value,
-        });
+        const {
+            name,
+            value,
+            type,
+            checked,
+        } = event.target;
+        if (type === 'checkbox') {
+            this.setState({
+                [name]: checked,
+            });
+        } else {
+            this.setState({
+                [name]: value,
+            });
+        }
     }
 
     render() {
-        const { firstName, lastName } = this.state;
+        const {
+            firstName,
+            lastName,
+            isFriendly,
+            gender,
+            color,
+        } = this.state;
         return (
             <form className="centered-with-margin">
                 <input
@@ -35,11 +55,77 @@ class Input extends React.Component {
                     placeholder="Last Name"
                     onChange={this.handleChange}
                 />
-                <h1>
-                    {firstName}
-                    &nbsp;
-                    {lastName}
-                </h1>
+                <label htmlFor="friendly-checkbox">
+                    <input
+                        id="friendly-checkbox"
+                        type="checkbox"
+                        name="isFriendly"
+                        checked={isFriendly}
+                        onChange={this.handleChange}
+                    />
+                    Is friendly?
+                </label>
+                <label htmlFor="male-radio">
+                    <input
+                        id="male-radio"
+                        type="radio"
+                        name="gender"
+                        value="Male"
+                        checked={gender === 'Male'}
+                        onChange={this.handleChange}
+                    />
+                    Male
+                </label>
+                <label htmlFor="female-radio">
+                    <input
+                        id="female-radio"
+                        type="radio"
+                        name="gender"
+                        value="Female"
+                        checked={gender === 'Female'}
+                        onChange={this.handleChange}
+                    />
+                    Female
+                </label>
+                <label htmlFor="favorite-color">
+                    Favorite color:
+                    <select id="favorite-color" value={color} name="color" onChange={this.handleChange}>
+                        <option value="Blue">Blue</option>
+                        <option value="Green">Green</option>
+                        <option value="Red">Red</option>
+                    </select>
+                </label>
+                {
+                    firstName.length > 0 && lastName.length > 0 && (
+                        <h1>
+                            {firstName}
+                            &nbsp;
+                            {lastName}
+                            &nbsp;
+                            is
+                            &nbsp;
+                            {isFriendly ? 'Friendly' : 'Unfriendly'}
+                        </h1>
+                    )
+                }
+                {
+                    gender.length > 0 && (
+                        <h2>
+                            You are:
+                            &nbsp;
+                            {gender}
+                        </h2>
+                    )
+                }
+                {
+                    color.length > 0 && (
+                        <h2>
+                            Favorite color:
+                            &nbsp;
+                            {color}
+                        </h2>
+                    )
+                }
             </form>
         );
     }
